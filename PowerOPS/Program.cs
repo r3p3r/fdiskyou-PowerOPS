31fa6de53650e40d1e2ed833dc50f9bfdacb1d33
fdiskyou <fdiskyou@users.noreply.github.com>
2016-05-25 21:43:20 +0100

    new modules and full powershell shell

--- PowerOPS/Program.cs
+++ PowerOPS/Program.cs
@@ -22,7 +22,6 @@ namespace PowerOPS
             			
         }
 
-        //The Methods can be Uninstall/Install.  Install is transactional, and really unnecessary.
         public override void Uninstall(System.Collections.IDictionary savedState)
         {
             Program.Main();
@@ -35,89 +34,33 @@ namespace PowerOPS
         {
             Console.Clear();
             Console.BackgroundColor = ConsoleColor.DarkBlue;
-            Console.WriteLine(@"PowerOPS - PowerShell for Offensive Operations"); Console.ResetColor(); Console.WriteLine();
+            Console.WriteLine(@"PowerOPS v0.5 - PowerShell for Offensive Operations"); Console.ResetColor(); Console.WriteLine();
         }
 
         public static void DisplayModules()
         {
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine("\n[*] PowerUp\n"); Console.ResetColor();
-            Console.Write(" Get-ApplicationHost            Get-UnattendedInstallFiles   Invoke-ServiceDisable          Write-CMDServiceBinary\n");
-            Console.Write(" Get-RegAlwaysInstallElevated   Get-Webconfig                Invoke-ServiceEnable           Write-ServiceEXE\n");
-            Console.Write(" Get-RegAutoLogon               Invoke-AllChecks             Invoke-ServiceStart            Write-ServiceEXECMD\n");
-            Console.Write(" Get-ServiceDetails             Invoke-FindDLLHijack         Invoke-ServiceStop             Get-ServicePerms\n");
-            Console.Write(" Get-ServiceEXEPerms            Invoke-FindPathHijack        Invoke-ServiceUserAdd          Write-UserAddMSI\n");
-            Console.Write(" Get-ServiceUnquoted            Invoke-ServiceCMD            Restore-ServiceEXE             Write-UserAddServiceBinary\n");
+            Console.ForegroundColor = ConsoleColor.Green; if (!isDomainJoined()) Console.WriteLine("\n[-] This computer is not part of a Domain! Some functions will not work!"); Console.ResetColor();
 
             Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.Write("\n[*] PowerView"); if (!isDomainJoined()) Console.WriteLine(" - This computer is not part of a Domain! Most of the PowerView functions will not work!"); Console.ResetColor();
-            Console.Write("\n Get-HostIP                     Get-NetForest                Get-NetUsers                   Invoke-NetGroupUserAdd\n");
-            Console.Write(" Get-LastLoggedOn               Get-NetForestDomains         Get-NetForestTrusts            Invoke-NetUserAdd\n");
-            Console.Write(" Get-NetComputers               Get-NetGroup                 Get-UserProperties             Invoke-Netview\n");
-            Console.Write(" Get-NetConnections             Get-NetGroups                Invoke-CheckLocalAdminAccess   Invoke-SearchFiles\n");
-            Console.Write(" Get-NetCurrentUser             Get-NetLocalGroup            Invoke-CheckWrite              Invoke-ShareFinder\n");
-            Console.Write(" Get-NetDomain                  Get-NetLocalGroups           Invoke-CopyFile                Invoke-StealthUserHunter\n");
-            Console.Write(" Get-NetDomainControllers       Get-NetLocalServices         Invoke-EnumerateLocalAdmins    Invoke-UserFieldSearch\n");
-            Console.Write(" Get-NetDomainTrusts            Get-NetLoggedon              Invoke-FileFinder              Invoke-UserHunter\n");
-            Console.Write(" Get-NetFiles                   Get-NetSessions              Invoke-FindLocalAdminAccess    Set-MacAttribute\n");
-            Console.Write(" Get-NetFileServers             Get-NetShare                 Invoke-FindVulnSystems         Test-Server\n");
-            Console.Write(" Get-NetFileSessions            Get-NetUser                  Invoke-HostEnum\n");
+            Console.WriteLine("\n[+] Nishang\n"); Console.ResetColor();
+            Console.Write(" Get-Information    Get-PassHashes             Port-Scan\n");
 
             Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine("\n[*] Nishang\n"); Console.ResetColor();
-            Console.Write(" Get-Information                Get-PassHashes               Port-Scan\n");
+            Console.WriteLine("\n[+] PowerSploit\n"); Console.ResetColor();
+            Console.Write(" Get-KeyStrokes     Invoke-DllInjection        Invoke-Mimikatz     Invoke-NinjaCopy\n");
+            Console.Write(" Invoke-Shellcode   Invoke-TokenManipulation   Invoke-WmiCommand   Invoke-ReflectivePEInjection\n");
+            Console.Write(" PowerView          PowerUp\n");
 
             Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine("\n[*] PowerSploit\n"); Console.ResetColor();
-            Console.Write(" Invoke-DllInjection            Invoke-Mimikatz              Invoke-NinjaCopy               Invoke-ReflectivePEInjection\n");
-            Console.Write(" Invoke-Shellcode               Invoke-TokenManipulation     Invoke-WmiCommand\n");
+            Console.WriteLine("\n[+] Empire\n"); Console.ResetColor();
+            Console.Write(" Invoke-PsExec      Invoke-SSHCommand\n");
 
             Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine("\n[*] Others\n"); Console.ResetColor();
-            Console.Write(" Auto-GPPPassword               Invoke-PsExec                PowerCat\n");
+            Console.WriteLine("\n[+] Others\n"); Console.ResetColor();
+            Console.Write(" Auto-GPPPassword   Get-ProductKey             PowerCat\n");
 
             Console.ResetColor();
-
             Console.WriteLine("");
-        }
-
-        public static void PSinteract()
-        {
-            PowerShellExecutor t = new PowerShellExecutor();
-            string PSfunction = null;
-
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.Write("\nAll modules imported. Any valid powershell command entered bellow will be executed.\n\n");
-            Console.ResetColor();
-                
-            do
-            {
-                Console.Write("PS > ");
-                PSfunction = Console.ReadLine();
-
-                if (PSfunction == "exit")
-                    return;
-
-                if (PSfunction.IndexOf("Invoke-Mimikatz", StringComparison.OrdinalIgnoreCase) >= 0)
-                    if (!IsUserAdministrator())
-                        return;
-                if (PSfunction.IndexOf("Get-PassHashes", StringComparison.OrdinalIgnoreCase) >= 0)
-                    if (!IsUserAdministrator())
-                        return;
-
-                if (PSfunction.IndexOf("Invoke-Shellcode", StringComparison.OrdinalIgnoreCase) >= 0)
-                    if (!isArch_x86())
-                        return;
-
-                try
-                {
-                    t.ExecuteSynchronously(PSfunction);
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("{0}", e.Message);
-                }
-            } while (PSfunction != "exit");
         }
 
         public static bool isDomainJoined()
@@ -141,7 +84,7 @@ namespace PowerOPS
             else
             {
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("\nSorry, this module is only for x86 archs.");
+                Console.WriteLine("\nSorry, this module is only for x86.\n");
                 Console.ResetColor();
                 return false;
             }
@@ -172,9 +115,8 @@ namespace PowerOPS
             }
             if (isAdmin == false)
             {
-                Console.Clear();
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("\nSorry, you need Admin privileges to perform this action.");
+                Console.WriteLine("\nSorry, you need Admin privileges to perform this action.\n");
                 Console.ResetColor();
                 return false;
             }
@@ -191,40 +133,83 @@ namespace PowerOPS
 
         public static void Main()
         {
-            Console.Title = "PowerOPS - PowerShell for Offensive Operations";
-            Console.SetWindowSize(Math.Min(122, Console.LargestWindowWidth), Math.Min(45, Console.LargestWindowHeight));
+            Console.Title = "PowerOPS - rui@deniable.org";
+            Console.SetWindowSize(Math.Min(122, Console.LargestWindowWidth), Math.Min(40, Console.LargestWindowHeight));
             Console.SetBufferSize(Console.BufferWidth, Console.BufferHeight);
 
             string command = null;
             DisplayBanner();
-            Console.WriteLine("Type 'help' to list available options\n");
+            Console.WriteLine("Type 'show' to list available modules\n");
+
+            Runspace runspace = RunspaceFactory.CreateRunspace();
+            runspace.Open();
 
             do
             {
-                Console.Write("\nOPS > ");
+                Console.Write("PS > ");
                 command = Console.ReadLine();
 
-                switch(command)
+                switch (command)
                 {
-                    case "help":
-                        Console.WriteLine("\nVersion 0.1b - rui@deniable.org\n\n[*] OPTIONS\n");
-                        Console.ForegroundColor = ConsoleColor.Green; Console.Write("  help"); Console.ResetColor(); Console.WriteLine("                  Display Help");
-                        Console.ForegroundColor = ConsoleColor.Green; Console.Write("  show modules"); Console.ResetColor(); Console.WriteLine("          List All Available Modules And Functions");
-                        Console.ForegroundColor = ConsoleColor.Green; Console.Write("  powershell"); Console.ResetColor(); Console.WriteLine("            Enter PowerShell shell");
-                        Console.ForegroundColor = ConsoleColor.Green; Console.Write("  exit"); Console.ResetColor(); Console.WriteLine("                  Exit this program");
-                        break;
-                    case "show modules":
+                    case "show":
                         DisplayModules();
                         break;
-                    case "powershell":
-                        PSinteract();
-                        break;
+                    case "exit":
+                        return;
                     default:
-                        Console.WriteLine("\nCommand not found.");
+                        if (command.IndexOf("Invoke-Mimikatz", StringComparison.OrdinalIgnoreCase) >= 0)
+                            if (!IsUserAdministrator())
+                                break;
+                        if (command.IndexOf("Get-PassHashes", StringComparison.OrdinalIgnoreCase) >= 0)
+                            if (!IsUserAdministrator())
+                                break;
+                        if (command.IndexOf("Invoke-Shellcode", StringComparison.OrdinalIgnoreCase) >= 0)
+                            if (!isArch_x86())
+                                break;
+
+                        try
+                        {  
+                            Pipeline pipeline = runspace.CreatePipeline();
+                            pipeline.Commands.AddScript(PowerOPS.GetKeyStrokes());
+                            pipeline.Commands.AddScript(PowerOPS.InvokeDLLInjection());
+                            pipeline.Commands.AddScript(PowerOPS.InvokeMimikatz());
+                            pipeline.Commands.AddScript(PowerOPS.InvokeNinjaCopy());
+                            pipeline.Commands.AddScript(PowerOPS.InvokeReflectivePEInjection());
+                            pipeline.Commands.AddScript(PowerOPS.InvokeShellcode());
+                            pipeline.Commands.AddScript(PowerOPS.InvokeTokenManipulation());
+                            pipeline.Commands.AddScript(PowerOPS.InvokeWMICommand());
+                            pipeline.Commands.AddScript(PowerOPS.PowerUp());
+                            pipeline.Commands.AddScript(PowerOPS.PowerView());
+                            pipeline.Commands.AddScript(PowerOPS.Nishang_GetInformation());
+                            pipeline.Commands.AddScript(PowerOPS.Nishang_GetPassHashes());
+                            pipeline.Commands.AddScript(PowerOPS.Nishang_PortScan());
+                            pipeline.Commands.AddScript(PowerOPS.AutoGPPPassword());
+                            pipeline.Commands.AddScript(PowerOPS.PowerCat());
+                            pipeline.Commands.AddScript(PowerOPS.GetProductKey());
+                            pipeline.Commands.AddScript(PowerOPS.Empire_InvokePSExec());
+                            pipeline.Commands.AddScript(PowerOPS.Empire_InvokeSshCommand());
+                            pipeline.Commands.AddScript(command);
+                            pipeline.Commands.Add("Out-String");
+                            Collection<PSObject> results = pipeline.Invoke();
+                             
+                            StringBuilder stringBuilder = new StringBuilder();
+                            foreach (PSObject obj in results)
+                            {
+                                stringBuilder.AppendLine(obj.ToString());
+                            }
+                            Console.Write(stringBuilder.ToString());
+                            
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("{0}", e.Message);
+                        }        
                         break;
                 }
+
             } while (command != "exit");
 
+            runspace.Close();
             Environment.Exit(0);
         }
 
